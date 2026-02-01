2ae6fa353a279f9b8df704a6437d0496c071f91e
Raj Suriyan G <rajsuriyan200110@gmail.com>
2026-02-02 00:43:32 +0530

    Fix the profile button

--- frontend/src/components/NavbarMod.tsx
+++ frontend/src/components/NavbarMod.tsx
@@ -74,7 +74,7 @@ export default function NavbarMod() {
             <Button className={(loggedIn)?"hidden":""} as={Link} color="warning" href="/signup" variant="flat">
               Sign Up
             </Button>
-            {loggedIn && (<Button className={(loggedIn)?"":"hidden"} as={Link} color="warning" href="/logout" variant="flat">
+            {loggedIn && (<Button className={(loggedIn)?"":"hidden"} as={Link} color="warning" href="/profile" variant="flat">
               Profile
             </Button>)}
             <Button className={(loggedIn)?"":"hidden"} as={Link} color="warning" href="/logout" variant="flat">
