c570ae17aef5797712c28b3c1d1827004ed4a738
Raj Suriyan G <rajsuriyan200110@gmail.com>
2026-02-03 20:13:59 +0530

    Remove the useState Hooks

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,12 +1,6 @@
 import ProtectedRoute from "@/components/ProtectedRoute";
-import { useState } from "react";
 function Profile(){
-    const [click,onClicked] = useState(false);
-
-    const onChange = async () =>{
-        onClicked(true);
-       setTimeout(()=>{onClicked(false)},1000);
-    }
+   
 
     return (
         <ProtectedRoute>
